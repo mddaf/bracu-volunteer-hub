eeced4dcaef583023e4dad7a369bf6bed9ca6b05
Himel38 <jchimel102@gmail.com>
2025-04-18 05:05:05 +0600

    fix:rounting

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
-
+import HomePage from './pages/HomePage';
+import AccountPage from './pages/AccountPage';
 
 const App = () => {
+  const isLoggedIn = localStorage.getItem("user"); // simple login check
 
   return (
     <Router>
       <Routes>
+        <Route path="/" element={isLoggedIn ? <HomePage /> : <Navigate to="/login" />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/account" element={isLoggedIn ? <AccountPage /> : <Navigate to="/login" />} />
       </Routes>
     </Router>
   );
