ac28fd5c0adc885a3b3e783004ef97f0bd2e8944
TayebaHaque <tayeba.haque.chowdhury@g.bracu.ac.bd>
2025-05-04 19:48:31 +0600

    feat: homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
+// import { motion } from "framer-motion";
+
+
+import Navbar from "../components/Navbar";
 
 const HomePage = () => {
-  return (
-    <>
-      <Navbar />
-      <h1 style={{ textAlign: 'center' }}>Welcome to the Homepage!</h1>
-    </>
-  );
-};
 
+	return (
+		<div>
+			<Navbar />
+		</div>
+	);
+};
 export default HomePage;
